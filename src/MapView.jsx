4ea5a2f970188d5d612478e95862e3ed49422edb
sahithi_09 <sri.ssm09@gmail.com>
2025-12-06 22:28:19 +0530

    Update MapView.jsx

--- src/MapView.jsx
+++ src/MapView.jsx
@@ -1,40 +1,58 @@
-import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet"
-import "leaflet/dist/leaflet.css"
-import L from "leaflet"
-import { useEffect } from "react"
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
+import L from "leaflet";
+import { useEffect } from "react";
 
 function MapView({ tasks }) {
-  // Fix default marker icon issue in Leaflet
-  useEffect(() => {
-    delete L.Icon.Default.prototype._getIconUrl
-    L.Icon.Default.mergeOptions({
-      iconRetinaUrl: "https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon-2x.png",
-      iconUrl: "https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon.png",
-      shadowUrl: "https://unpkg.com/leaflet@1.9.4/dist/images/marker-shadow.png",
-    })
-  }, [])
+  const safeTasks = Array.isArray(tasks) ? tasks : [];
 
-  const defaultPosition = [tasks[0]?.lat || 20.5937, tasks[0]?.lng || 78.9629] // fallback to India
+  useEffect(() => {
+    delete L.Icon.Default.prototype._getIconUrl;
+    L.Icon.Default.mergeOptions({
+      iconRetinaUrl:
+        "https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon-2x.png",
+      iconUrl:
+        "https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon.png",
+      shadowUrl:
+        "https://unpkg.com/leaflet@1.9.4/dist/images/marker-shadow.png",
+    });
+  }, []);
+
+  const defaultPosition = [
+    safeTasks[0]?.latitude || 20.5937,
+    safeTasks[0]?.longitude || 78.9629,
+  ];
 
   return (
     <div className="map-view-container">
-      <MapContainer center={defaultPosition} zoom={5} scrollWheelZoom={true} style={{ height: "600px", width: "100%" }}>
+      <MapContainer
+        center={defaultPosition}
+        zoom={5}
+        scrollWheelZoom
+        style={{ height: "600px", width: "100%" }}
+      >
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a>'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
-        {tasks.map((task) => (
-          <Marker key={task.id} position={[task.lat, task.lng]}>
+
+        {safeTasks.map((task) => (
+          <Marker
+            key={task._id}
+            position={[task.latitude, task.longitude]}
+          >
             <Popup>
-              <strong>{task.Name}</strong><br />
-              {task.Location}<br />
-              {task.Status} - {task.ConnectorType}
+              <strong>{task.name}</strong>
+              <br />
+              {task.address}
+              <br />
+              {task.status} - {task.connectortype}
             </Popup>
           </Marker>
         ))}
       </MapContainer>
     </div>
-  )
+  );
 }
 
-export default MapView
+export default MapView;
